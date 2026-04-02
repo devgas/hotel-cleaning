bb4cd5ff959c0c3fb205a23e288add6f2caa94a4
a.havriushov <a.havriushov@gmail.com>
2026-04-02 23:02:16 +0200

    revert: remove PWA install prompt

--- src/app/[locale]/(app)/layout.tsx
+++ src/app/[locale]/(app)/layout.tsx
@@ -1,5 +1,4 @@
 import { BottomNav } from '@/components/common/BottomNav'
-import { InstallPrompt } from '@/components/common/InstallPrompt'
 import { auth } from '@/lib/auth/authOptions'
 import { redirect } from 'next/navigation'
 
@@ -18,7 +17,6 @@ export default async function AppLayout({
     <div className="min-h-screen bg-gray-50">
       <main className="pb-20">{children}</main>
       <BottomNav locale={locale} />
-      <InstallPrompt />
     </div>
   )
 }
